e7edc1b0f58e0b77013cae250ed956242457e239
Stuart Ellis <stuart@stuartellis.name>
2022-10-16 16:57:57 +0000

    Amend Trivy config

--- make/tools/trivy-cli.mk
+++ make/tools/trivy-cli.mk
@@ -5,16 +5,20 @@
 # Trivy Docker Container
 
 TRIVY_CMD_DOCKER_IMAGE		:= aquasec/trivy:$(TRIVY_VERSION)
-TRIVY_CACHE_DIR				:= ./tmp/cache/trivy
 
 # Other Project Variables for Trivy
 
-TRIVY_SRC_HOST_DIR		:= $(HOST_PROJECT_PATH)
+TRIVY_CONFIG_FILE			:= ./trivy.yaml
+TRIVY_CACHE_DIR				:= ./tmp/cache/trivy
+TRIVY_OUTPUT_DIR			:= ./tmp/output/trivy
+TRIVY_OUTPUT_FILE			:= trivy-report.txt
+TRIVY_OUTPUT_FORMAT			:= table
+TRIVY_SRC_HOST_DIR			:= $(HOST_PROJECT_PATH)
 
 TRIVY_RUN_CMD := --user $(shell id -u) \
  	--mount type=bind,source=$(TRIVY_SRC_HOST_DIR),destination=$(SRC_BIND_DIR) \
  	-w $(SRC_BIND_DIR) \
- 	$(TRIVY_CMD_DOCKER_IMAGE) --skip-dirs $(TRIVY_CACHE_DIR) --cache-dir $(TRIVY_CACHE_DIR)
+ 	$(TRIVY_CMD_DOCKER_IMAGE) --cache-dir $(TRIVY_CACHE_DIR) --format $(TRIVY_OUTPUT_FORMAT) --output $(TRIVY_OUTPUT_DIR)/$(TRIVY_OUTPUT_FILE) --config $(TRIVY_CONFIG_FILE)
 
 # Trivy Targets
 
@@ -24,11 +28,9 @@ trivy-info:
 
 .PHONY trivy-code:
 trivy-code:
+	@mkdir -p $(TRIVY_CACHE_DIR)
+	@mkdir -p $(TRIVY_OUTPUT_DIR)
 	@$(DOCKER_RUN_CMD) $(TRIVY_RUN_CMD) config .
-
-.PHONY trivy-scan:
-trivy-scan:
-	@$(DOCKER_RUN_CMD) $(TRIVY_RUN_CMD) fs .
 
 .PHONY trivy-shell:
 trivy-shell:
